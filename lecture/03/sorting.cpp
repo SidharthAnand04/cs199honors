7d97f1966dd576ddb6fa0e93728efe017815ea06
Sidharth Anand <narwhals2004@gmail.com>
2023-10-08 00:44:57 +0000

    finish sort

--- lecture/03/sorting.cpp
+++ lecture/03/sorting.cpp
@@ -1,3 +1,86 @@
+// #include <algorithm>
+// #include <climits>
+// #include <chrono>
+// #include <cstring>
+// #include <iostream>
+// #include <random>
+// #include <vector>
+
+// #define SIZE 100
+
+// template<typename T>
+// void print_vector(std::vector<T> &list) {
+//     for( int num : list) {
+//         std::cout << num << " ";
+//     }
+//     std::cout << std::endl;
+// }
+
+// int main(int argc, char **argv) {
+//     if(argc != 2) {
+//         std::cout << "random: must specify number" << std::endl;
+//         return (404);
+//     }
+//     int number = atoi(argv[1]);
+
+//     auto start{std::chrono::steady_clock::now()};
+//     auto end{std::chrono::steady_clock::now()};
+
+//     start = std::chrono::steady_clock::now();
+
+//     std::vector<int> list(number);
+//     // std::vector<int> list;
+
+//     std::random_device rd;  // a seed source for the random number engine
+//     std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+//     std::uniform_int_distribution<> randint(1, INT_MAX);
+
+//     for(int i = 0; i < number; ++i) {
+//         list[i] = randint(gen)%2;
+//     }
+//     // for(int i = 0; i < number; ++i) {
+//     //     list.push_back(randint(gen) % SIZE);
+//     // }
+
+//     auto list2 = list;
+
+//     end = std::chrono::steady_clock::now();
+//     std::chrono::duration<double> elapsed_seconds_init{end - start};
+
+//     std::cout << "Initialization time = " << elapsed_seconds_init.count() << "s\n";
+
+//     // print_vector(list);
+//     start = std::chrono::steady_clock::now();
+//     std::sort(list.begin(), list.end());
+//     end = std::chrono::steady_clock::now();
+
+//     std::chrono::duration<double> elapsed_seconds_sort{end - start};
+//     std::cout << "Sort time = " << elapsed_seconds_sort.count() << "s\n";
+
+//     start = std::chrono::steady_clock::now();
+//     int count = 0;
+//     for(auto num : list2) {
+//         count += num;
+//     }
+
+//     memset(&list2[0], 0, sizeof(int)*(list2.size() - count) );
+//     for(int i = (list2.size() - count); i < list2.size(); ++i) {
+//         list2[i] = 1;
+//     }
+
+//     end = std::chrono::steady_clock::now();
+
+//     std::chrono::duration<double> elapsed_seconds_sort2{end - start};
+//     std::cout << "Sort2 time = " << elapsed_seconds_sort2.count() << "s\n";
+
+//     if(list == list2) {
+//         std::cout << "works" << std::endl;
+//     }
+//     // print_vector(list);
+
+//     return 0;
+// }
+
 #include <algorithm>
 #include <climits>
 #include <chrono>
@@ -8,18 +91,44 @@
 
 #define SIZE 100
 
-template<typename T>
-void print_vector(std::vector<T> &list) {
-    for( int num : list) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
+struct HSLA {
+  float hue;
+  float saturation;
+  float lightness;
+  float alpha;
+};
+
+// Calculates the luminosity of an HSLA pixel value.
+float calculate_luminosity(const HSLA& hsla) {
+  return (hsla.lightness + hsla.saturation) / 2.0f;
+}
+
+// Compares two HSLA pixel values based on their luminosity.
+bool compare_hsla_luminosity(const HSLA& hsla1, const HSLA& hsla2) {
+  return calculate_luminosity(hsla1) < calculate_luminosity(hsla2);
+}
+
+// Radix sort for sorting HSLA pixel values based on luminosity.
+void radix_sort_hsla_luminosity(std::vector<HSLA>& list) {
+  // Create an array of buckets, one for each luminosity value.
+  std::vector<std::vector<HSLA>> buckets(256);
+
+  // Iterate over the list of HSLA pixel values and add each pixel value to the bucket that corresponds to its luminosity.
+  for (const HSLA& hsla : list) {
+    buckets[static_cast<int>(calculate_luminosity(hsla) * 255.0f)].push_back(hsla);
+  }
+
+  // Iterate over the buckets and append the contents of each bucket to the sorted list.
+  list.clear();
+  for (int i = 0; i < buckets.size(); ++i) {
+    list.insert(list.end(), buckets[i].begin(), buckets[i].end());
+  }
 }
 
 int main(int argc, char **argv) {
-    if(argc != 2) {
-        std::cout << "random: must specify number" << std::endl;
-        return (404);
+    if (argc != 2) {
+    std::cout << "random: must specify number" << std::endl;
+    return (404);
     }
     int number = atoi(argv[1]);
 
@@ -28,55 +137,48 @@ int main(int argc, char **argv) {
 
     start = std::chrono::steady_clock::now();
 
-    std::vector<int> list(number);
-    // std::vector<int> list;
+    std::vector<HSLA> list(number);
 
     std::random_device rd;  // a seed source for the random number engine
     std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> randint(1, INT_MAX);
+    std::uniform_real_distribution<> rand_hue(0.0f, 360.0f);
+    std::uniform_real_distribution<> rand_saturation(0.0f, 1.0f);
+    std::uniform_real_distribution<> rand_lightness(0.0f, 1.0f);
+    std::uniform_real_distribution<> rand_alpha(0.0f, 1.0f);
 
-    for(int i = 0; i < number; ++i) {
-        list[i] = randint(gen)%2;
+    for (int i = 0; i < number; ++i) {
+    list[i] = {rand_hue(gen), rand_saturation(gen), rand_lightness(gen), rand_alpha(gen)};
     }
-    // for(int i = 0; i < number; ++i) {
-    //     list.push_back(randint(gen) % SIZE);
-    // }
-
-    auto list2 = list;
 
     end = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed_seconds_init{end - start};
 
     std::cout << "Initialization time = " << elapsed_seconds_init.count() << "s\n";
 
-    // print_vector(list);
+    // Sort the list using the std::sort algorithm.
     start = std::chrono::steady_clock::now();
-    std::sort(list.begin(), list.end());
+    std::sort(list.begin(), list.end(), compare_hsla_luminosity);
     end = std::chrono::steady_clock::now();
 
-    std::chrono::duration<double> elapsed_seconds_sort{end - start};
-    std::cout << "Sort time = " << elapsed_seconds_sort.count() << "s\n";
+    std::chrono::duration<double> elapsed_seconds_sort_std{end - start};
+    std::cout << "std::sort time = " << elapsed_seconds_sort_std.count() << "s\n";
 
+    // Sort the list using the radix sort algorithm.
     start = std::chrono::steady_clock::now();
-    int count = 0;
-    for(auto num : list2) {
-        count += num;
-    }
-
-    memset(&list2[0], 0, sizeof(int)*(list2.size() - count) );
-    for(int i = (list2.size() - count); i < list2.size(); ++i) {
-        list2[i] = 1;
-    }
-
+    radix_sort_hsla_luminosity(list);
     end = std::chrono::steady_clock::now();
 
-    std::chrono::duration<double> elapsed_seconds_sort2{end - start};
-    std::cout << "Sort2 time = " << elapsed_seconds_sort2.count() << "s\n";
+    std::chrono::duration<double> elapsed_seconds_sort_radix{end - start};
+    std::cout << "radix sort time = " << elapsed_seconds_sort_radix.count() << "s\n";
 
-    if(list == list2) {
-        std::cout << "works" << std::endl;
+    if (elapsed_seconds_sort_std.count() < elapsed_seconds_sort_radix.count()) {
+    std::cout << "std::sort is faster" << std::endl;
+    } else if (elapsed_seconds_sort_std.count() > elapsed_seconds_sort_radix.count()) {
+    std::cout << "Radix sort is faster" << std::endl;
+    } else {
+    std::cout << "The two sorting algorithms have the same performance" << std::endl;
     }
-    // print_vector(list);
 
+    std::cout << number << " " << " " << elapsed_seconds_sort_std.count() << " " << elapsed_seconds_sort_radix.count() << std::endl;
     return 0;
 }
